4d2ad8b41fc804256fd77d3bcf0f4a71d4e2e461
nakyoung98 <jay3925@cau.ac.kr>
2024-08-14 13:55:13 +0900

    Feat: main db 연결

--- eastar-be/src/app.module.ts
+++ eastar-be/src/app.module.ts
@@ -3,9 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    //env
+    ConfigModule.forRoot(),
+
+    //crud
+    AuthModule,
+    UserModule,
+
+    //db
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MAIN_DB_HOST,
+      port: parseInt(process.env.MAIN_DB_PORT),
+      username: process.env.MAIN_DB_USER_NAME,
+      password: process.env.MAIN_DB_USER_PASSWORD,
+      database: process.env.MAIN_DB_USER_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
